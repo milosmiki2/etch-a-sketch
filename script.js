4aaa57b767e92574d6a00defd16127cce734e5e4
Milos Manojlovic <malidzidza@gmail.com>
2025-06-09 12:59:46 +0200

    Found the logic!

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 "use strict";
 
 const grid = document.querySelector(".grid");
-for (let i = 0; i < 16; i++) {
+
+// let div = document.createElement("div");
+for (let i = 0; i < 16 * 16; i++) {
   const div = document.createElement("div");
-  // div.setAttribute("class", "square");
+  div.setAttribute("class", "square");
   grid.appendChild(div);
+  div.addEventListener("mouseenter", () => {
+    div.setAttribute("style", "background-color: black");
+  });
 }
 
 console.log(grid);
